4e93343c2595cce4c6b8ab101170ded13feed7f5
Dharshan-CODE <73929847+Dharshan-CODE@users.noreply.github.com>
2020-12-09 20:39:23 +0530

    Add files via upload

---
+++ green.js
@@ -0,0 +1,25 @@
+class green extends BaseClass{
+    constructor(x,y){
+      super(x,y,30,30);
+     
+    }
+  
+    display() {
+      if(this.body.speed<3){
+        fill('green');
+        stroke("white");
+        super.display();
+      }
+     else{
+       World.remove(world,this.body);
+       push();
+       this.Visibility = this.Visibility-5;
+       tint(255,this.Visibility);
+     
+       pop();
+     }
+      
+      
+    }
+  }
+  
---
+++ yellow.js
@@ -0,0 +1,28 @@
+class yellow extends BaseClass{
+    constructor(x,y){
+      super(x,y,30,30);
+      this.Visibility = 255;
+    }
+  
+    display() {
+    
+      if(this.body.speed<3){
+        fill('yellow');
+        stroke("white");
+        super.display();
+      }
+     else{
+       World.remove(world,this.body);
+       push();
+       this.Visibility = this.Visibility-5;
+       tint(255,this.Visibility);
+     
+       pop();
+     }
+      
+     
+  }
+}
+  
+
+  
